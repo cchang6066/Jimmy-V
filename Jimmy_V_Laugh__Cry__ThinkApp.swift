c8d813dcab2bb9db808234faedc42a9247ffcecf
Dylan B <162370301+DylpickleP@users.noreply.github.com>
2024-11-04 09:21:06 -0600

    Add files via upload

---
+++ Jimmy_V_Laugh__Cry__ThinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jimmy_V_Laugh__Cry__ThinkApp.swift
+//  Jimmy V Laugh, Cry, Think
+//
+//  Created by Dylan Borgio on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Jimmy_V_Laugh__Cry__ThinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
